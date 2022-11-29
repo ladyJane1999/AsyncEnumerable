b020b07e31df0ae432f4f846de344dfd21cff3b4
aromanova <anzhela.romanova@altenar.com>
2022-11-29 12:33:40 +0300

    Add project files.

---
+++ AsyncEnumerable/Program.cs
@@ -0,0 +1,25 @@
+﻿   class Repository
+    {
+        List<string> data = new List<string>{ "Tom", "Sam", "Kate", "Alice", "Bob" };
+        public async IAsyncEnumerable<string> GetDataAsync()
+        {
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+                await Task.Delay(500);
+                yield return item;
+            }
+        }
+    }
+
+    public class Program { 
+    public static async Task Main(string[] args)
+    {
+        Repository repo = new Repository();
+        IAsyncEnumerable<string> data = repo.GetDataAsync();
+        await foreach (var name in data)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
